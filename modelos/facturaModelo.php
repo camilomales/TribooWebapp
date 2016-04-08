37b0340f01b7ba2fa22618e7ead8059d40e88178
pastorvio10 <cristibas10@hotmail.com>
2016-04-08 10:58:27 -0500

    Added files via upload

--- modelos/facturaModelo.php
+++ modelos/facturaModelo.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../app/modelo.php";
 require_once "../modelos/usuariosModelo.php";
+session_start();
 
 class facturaModelo extends Modelo{
 	public function __construct(){
@@ -12,6 +13,42 @@ class facturaModelo extends Modelo{
 		$result = $this->_db->query($sql);
 		if ($this->_db->error ){
 		   echo "Error al guardar el registro: ".$this->_db->error;
+		   return;		   
+		}		
+	}
+
+	public function listarFacturas(){
+		$idUsuario = $_SESSION['sesion']; 
+		$sql="SELECT * FROM validaciones where idUsuario='$idUsuario'";	
+		$result = $this->_db->query($sql);  
+		$registros = $result->fetch_all(MYSQLI_ASSOC);  
+		return $registros;	
+	}
+
+	public function listarCodExcel(){
+		$idUsuario = $_SESSION['sesion']; 
+		$sql="SELECT codValidacion FROM validaciones where idUsuario='$idUsuario'";		
+		$result = $this->_db->query($sql);  
+		$registros = $result->fetch_all(MYSQLI_ASSOC);  
+		return $registros;	
+
+	}
+
+	public function numFilas(){
+		$idUsuario = $_SESSION['sesion']; 
+		$sql="SELECT count(*) FROM validaciones";		
+		$result = $this->_db->query($sql);
+		$registros = $result->fetch_all(MYSQLI_ASSOC);  
+		return $registros;	
+
+	}
+
+	public function modificarFactura($idValidacion, $codValidacion, $fechaVenta, $montoVenta, $idUsuario){
+		$sql="UPDATE validaciones SET codValidacion='$codValidacion', fechaVenta='$fechaVenta', montoVenta='$montoVenta'
+			WHERE idValidacion= '$idValidacion'";	
+		$result = $this->_db->query($sql);
+		if ($this->_db->error ){
+		   echo "Error al actualizar el registro: ".$this->_db->error;
 		   return;		   
 		}		
 	}
